1ec6e518aeea56fe19460e8cb310c587dd9ec69b
Dmitry Klyushentsev <klyushentsev@gmail.com>
2021-07-02 12:47:54 +0300

    feat: finished morse decoder function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrExpr = expr.match(/.{1,10}/g);
+    arrExpr = arrExpr.map(symbol => {
+        if(symbol === '**********') return ' ';
+        symbol = Number(symbol).toString();
+        symbol = symbol.match(/.{1,2}/g);
+        symbol = symbol.map(code => {
+            if(code === '10') return '.';
+            else return '-';
+        });
+        return symbol.join('');
+    });
+    result = arrExpr.map(morseCode => {
+        if(morseCode === ' ') return ' ';
+        return MORSE_TABLE[morseCode];
+    }).join('');
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
